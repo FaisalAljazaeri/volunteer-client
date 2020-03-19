864a73b31e665ad62524eb5ffe83ea4bf975c950
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-03-19 05:51:01 +0300

    Change API URL

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -3,7 +3,7 @@ let apiURL;
 const expressPort = 5000;
 const apiUrls = {
   development: `http://localhost:${expressPort}/api`,
-  production: `https://aqueous-atoll-85096.herokuapp.com/api`,
+  production: `https://frozen-tor-65145.herokuapp.com/api`,
 
 }
 
